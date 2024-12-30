7c494d6e18f2d1066d13763f9c3c5e7edd027b2a
2q3w4 <2q3w4@TABLET-7DI70QFI>
2024-12-30 16:50:41 +0900

    q9498

---
+++ baekjoon/src/q9498/Main.java
@@ -0,0 +1,50 @@
+package q9498;
+
+
+import java.util.Scanner;
+public class Main {
+
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in);
+	        
+	        int inputNum = sc.nextInt();
+	        sc.close();
+	        
+	        if(inputNum>101)
+	        {
+	        	System.out.println("error");
+	        }
+	        else if(inputNum>=90)
+	        {
+	        	System.out.println('A');
+	        }
+	        else if(inputNum>=80)
+	        {
+	        	System.out.println('B');
+	        }
+	        else if(inputNum>=70)
+	        {
+	        	System.out.println('C');
+	        }
+	        else if(inputNum>=60)
+	        {
+	        	System.out.println('D');
+	        }
+	        else
+	        {
+	        	System.out.println('F');
+	        }
+	       
+	        /*switch(inputNum/10)
+	        {
+	        case 9:System.out.println('A');break;
+	        case 8:System.out.println('B');break;
+	        case 7:System.out.println('C');break;
+	        case 6:System.out.println('D');break;
+	        default:System.out.println('F');break;
+	        }*/
+		}
+	}
+
+
