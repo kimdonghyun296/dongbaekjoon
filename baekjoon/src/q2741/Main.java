e5324d8ae08f8cec531efff79ffa283ad622cf9e
2q3w4 <2q3w4@TABLET-7DI70QFI>
2024-12-30 15:18:33 +0900

    q2741

---
+++ baekjoon/src/q2741/Main.java
@@ -0,0 +1,19 @@
+package q2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        
+        int inputNum = sc.nextInt();
+        sc.close();
+        
+        for(int i=1;i<=inputNum;i++)
+        {
+        	System.out.println(i);
+        }
+	}
+}
