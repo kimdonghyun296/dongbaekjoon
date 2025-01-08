34aebe44fcbae14862e667655e95c0292d6842d3
2q3w4 <2q3w4@TABLET-7DI70QFI>
2025-01-08 17:20:35 +0900

    q9498

--- baekjoon/src/q9498/Main.java
+++ baekjoon/src/q9498/Main.java
@@ -35,7 +35,6 @@ public class Main {
 	        {
 	        	System.out.println('F');
 	        }
-	       
 	        /*switch(inputNum/10)
 	        {
 	        case 9:System.out.println('A');break;
